e2cc58510a5c43bbaaa83cff984150dd9d301b6c
jvilpoz1105 <jvilpoz1105@g.educaand.es>
2026-05-18 19:52:33 +0200

    Prueba de Funcionamiento

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "intellcar_vpc" {
   tags = { Name = "intellcar-vpc" }
 }
 
-# 2. Crear una Subred Pública primaria (Donde vivirá la EC2)
+# 2. Crear una Subred Pública primaria (Donde vivirá la EC2) 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.intellcar_vpc.id
   cidr_block        = "10.0.1.0/24"
